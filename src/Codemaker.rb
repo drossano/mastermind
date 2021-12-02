54ca321d54236a54f4866ba6b1c5328850c37a91
drossano <72749793+drossano@users.noreply.github.com>
2021-12-02 18:35:03 -0500

    Remove Array class from @code

--- src/Codemaker.rb
+++ src/Codemaker.rb
@@ -1,6 +1,6 @@
 class Codemaker
   def initialize
-    @code = Array[rand(1..6),rand(1..6), rand(1..6), rand(1..6)].join.to_i
+    @code = [rand(1..6),rand(1..6), rand(1..6), rand(1..6)].join.to_i
+    p @code
   end
 end
-
