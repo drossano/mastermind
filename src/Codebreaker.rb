de1ab2e3f4805f1e2b0438bcc79ef01a8c23bd98
drossano <72749793+drossano@users.noreply.github.com>
2021-12-02 22:01:55 -0500

    Add Codebreaker.rb to allow user to enter guesses

---
+++ src/Codebreaker.rb
@@ -0,0 +1,15 @@
+class Codebreaker
+  def initialize
+    puts "Enter 4 digits ranging from 1 to 6 to guess the code"
+    @guess = gets.chomp.to_s.split("").map{ |digit| digit.to_i}
+    loop do      
+      if @guess.all? { |digit| digit.between?(1,6)} && @guess.length == 4
+        break
+      else
+        puts "Invalid guess entered. Please enter 4 digits ranging from 1 to 6."
+        @guess = gets.chomp.to_s.split("").map{ |digit| digit.to_i}
+      end
+    end
+  end
+end
+
