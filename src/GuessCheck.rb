a74bc464afb1c09387c8b10f5dd7d62e15c7c381
drossano <72749793+drossano@users.noreply.github.com>
2021-12-09 21:22:24 -0500

    Fix check_numbers to properly display results

--- src/GuessCheck.rb
+++ src/GuessCheck.rb
@@ -3,9 +3,19 @@ require_relative "Codemaker"
 
 class GuessCheck
   def check_numbers(guess,code)
-    p code.filter{ |i| guess.include? i}.length
+    correct_numbers = 0
+    code_tally = code.tally
+    guess_tally = guess.tally
+    guess_tally.each do |guess_number, guess_frequency|
+      code_tally.each do |code_number, code_frequency|
+        if guess_number == code_number
+          correct_numbers += [guess_frequency,code_frequency].min
+        end
+      end
+    end
+    return correct_numbers
   end
-  
+
   def check_position(guess,code)
     correct_positions = guess.zip(code).count { |guess,code| guess == code }
     if correct_positions == 0
@@ -15,5 +25,3 @@ class GuessCheck
     end
   end
 end
-
-GuessCheck.new.check_numbers([1,2,1,5], [1,1,3,4])
