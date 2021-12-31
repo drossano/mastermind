b4bf732d2d3f046afb598bfe548f3701595b745a
Dean <72749793+drossano@users.noreply.github.com>
2021-12-31 10:41:25 -0500

    Move GameType.rb functionality into Game.rb

--- src/Game.rb
+++ src/Game.rb
@@ -1,40 +1,54 @@
 require_relative "GuessCheck"
-require_relative "GameType"
 require_relative "Codebreaker"
 require_relative "Codemaker"
 
 class Game
   def initialize
-    @game_type = GameType.new
-    if @game_type.game_type == 1
-      @code = Codemaker.new.player_code
-      @computer = Codebreaker.new
-    else
-      @code = Codemaker.new.computer_code
-      @player = Codebreaker.new
-    end
+    @game_type = get_game_type
     @check = GuessCheck.new
   end
 
+  def get_game_type
+    puts "Enter 1 if you would like to play as the Codemaker. Enter 2 if you would like to play as the Codebreaker."
+    game_type_choice = gets.chomp.to_i
+    loop do
+      if game_type_choice == 1 || game_type_choice == 2
+        return game_type_choice
+      else
+        puts "Invalid entry."
+        puts "Enter 1 if you would like to play as the Codemaker. Enter 2 if you would like to play as the Codebreaker."
+        game_type_choice = gets.chomp.to_i
+      end
+    end
+  end
+
   def guess(guess)
-    puts "#{@game_type.player_name} guessed #{guess.join("")}" if @game_type.game_type == 1
+    puts "#{player_name(@game_type)} guessed #{guess.join("")}" if @game_type == 1
     correct_numbers = @check.check_numbers(guess, @code)
     correct_positions = @check.check_position(guess, @code)
     if correct_positions == 4
       winner
     else
       puts number_feedback(correct_numbers) + position_feedback(correct_positions) + "."
-      @computer.code_solver(guess, correct_numbers,correct_positions) if @game_type.game_type == 1
+      @computer.code_solver(guess, correct_numbers,correct_positions) if @game_type == 1
+    end
+  end
+
+  def player_name(game_type)
+    if game_type == 1
+      "The computer"
+    elsif game_type == 2
+      "You"
     end
   end
 
   def winner
-    puts "#{@game_type.player_name} got the code right!"
+    puts "#{player_name(@game_type)} got the code right!"
       true
   end
 
   def number_feedback(correct_numbers)
-    "#{@game_type.player_name} #{correct_numbers} guessed #{@check.number_plurality(correct_numbers)} correctly"
+    "#{player_name(@game_type)} #{correct_numbers} guessed #{@check.number_plurality(correct_numbers)} correctly"
   end
 
   def position_feedback(correct_positions)
@@ -42,23 +56,34 @@ class Game
   end
 
   def turns
+    assign_roles(@game_type)
     i = 1
     max_turns = 12
     until i > max_turns
       puts "Turn #{i} of 12"
-      if @game_type.game_type == 1
+      if @game_type == 1
         guess = @computer.computer_guess(i)
         break if guess(guess) == true
 
         continue
-      elsif @game_type.game_type == 2
+      elsif @game_type == 2
         guess = @player.player_guess
         break if guess(guess) == true
       end
       i += 1
     end
     if i > max_turns
-      puts "#{@game_type.player_name} reached the maximum amount of turns. The code is #{@code.join}"
+      puts "#{player_name(@game_type)} reached the maximum amount of turns. The code is #{@code.join}"
+    end
+  end
+
+  def assign_roles(game_type)
+    if game_type == 1
+      @code = Codemaker.new.player_code
+      @computer = Codebreaker.new
+    else
+      @code = Codemaker.new.computer_code
+      @player = Codebreaker.new
     end
   end
 
